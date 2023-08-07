a61b26529b7bd118dec3b1be5cb4d0f4cf2797f4
Flávio Tassan <ftassan@outlook.com>
2023-08-07 10:13:15 -0300

    add board and column extensions

---
+++ Tasking/TrelloClone.Tasking.Domain/Extensions/BoardExtensions.cs
@@ -0,0 +1,10 @@
+﻿using TrelloClone.Tasking.Domain.Models;
+
+namespace TrelloClone.Tasking.Domain.Extensions;
+
+public static class BoardExtensions
+{
+    public static int CountColumns(this Board board) => board.Columns.Count;
+
+    public static int CountCards(this Board board) => board.Columns.Sum(column => column.CountCards());
+}
---
+++ Tasking/TrelloClone.Tasking.Domain/Extensions/ColumnExtensions.cs
@@ -0,0 +1,8 @@
+﻿using TrelloClone.Tasking.Domain.Models;
+
+namespace TrelloClone.Tasking.Domain.Extensions;
+
+public static class ColumnExtensions
+{
+    public static int CountCards(this Column column) => column.Cards.Count;
+}
