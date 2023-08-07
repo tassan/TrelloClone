7b198d57872d2f53f845dab06e62b9ee6c858b2d
Flávio Tassan <ftassan@outlook.com>
2023-08-07 10:59:41 -0300

    remove unused Cards property

--- Tasking/TrelloClone.Tasking.Domain/Models/Column.cs
+++ Tasking/TrelloClone.Tasking.Domain/Models/Column.cs
@@ -3,12 +3,10 @@
 public class Column : List<Card>
 {
     public string Name { get; set; }
-    public List<Card> Cards { get; set; }
 
     public Column()
     {
         Name = "New Column";
-        Cards = new List<Card>();
     }
     
     public Column(string columnName = "New Column") : this()
